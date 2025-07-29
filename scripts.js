e118ba608c5976c1d6b647663dd6776aa3307b90
Angela Gao <Xuan4781@users.noreply.github.com>
2025-07-29 15:24:04 -0400

    Defined two variables: countryCode and geocodeURL which is the API endpoint

--- scripts.js
+++ scripts.js
@@ -13,11 +13,14 @@ function fetchWeather() {
     `;
     return;
 
+    //gets location's lon and lat from a name
     async function getLonAndLat(){
-
+        const countryCode = 1;
+        const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`;
     }
+    //get current weather based on the coords
     async function getWeatherData(lon, lat){
-        
+
     }
   }
 
